811b85c0535f9e5589a52fbd85556152e7572a85
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2022-08-22 13:04:46 +0200

    Correction mineur view

--- views/view.py
+++ views/view.py
@@ -48,14 +48,12 @@ class View:
         birth_date = input("Entrez la date de naissance du joueur: ")
         gender = input("Entrez le sexe du joueur: ")
         rank = input("Entrez son classement: ")
-        score = 0
         return {
             "last_name": last_name,
             "first_name": first_name,
             "birth_date": birth_date,
             "gender": gender,
-            "rank": rank,
-            "score": score
+            "rank": rank
         }
         
         
